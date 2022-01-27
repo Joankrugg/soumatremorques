033148cee60676dadb86cb68a355a2d121295e6a
Philippe chambon <joan_krugg@live.fr>
2022-01-27 14:05:34 +0100

    new collection product pages

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,28 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def trailers
+    @products = Product.joins(:category).where(categories: { name: 'Trailers' })
+  end
+
+  def accessories
+    @products = Product.joins(:category).where(categories: { name: 'Accessories' })
+  end
+
+  def maintenance
+    @products = Product.joins(:category).where(categories: { name: 'Maintenance' })
+  end
+
+  def installations
+    @products = Product.joins(:category).where(categories: { name: 'Installations' })
+  end
+
+  def documents
+    @products = Product.joins(:category).where(categories: { name: 'Documents' })
+  end
+
+  def luggage
+    @products = Product.joins(:category, :subcategory).where(categories: { name: 'Trailers' }, subcategories: { name: 'Bagagères / Utilitaires' })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  resources :products, only: [:index, :show]
+  resources :products, only: [:index, :show] do
+    collection do
+      get :trailers
+      get :accessories
+      get :maintenance
+      get :installations
+      get :documents
+      get :luggage
+    end
+  end
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root to: 'pages#home'
