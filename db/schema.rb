e22fae250208da77af05a71935defd3c9a807e54
Philippe chambon <joan_krugg@live.fr>
2022-01-27 08:30:14 +0100

    add model Brand

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_26_210856) do
+ActiveRecord::Schema.define(version: 2022_01_27_071849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,12 @@ ActiveRecord::Schema.define(version: 2022_01_26_210856) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "brands", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
@@ -52,6 +58,8 @@ ActiveRecord::Schema.define(version: 2022_01_26_210856) do
     t.integer "max_load_weight"
     t.integer "empty_weight"
     t.bigint "category_id"
+    t.bigint "brand_id"
+    t.index ["brand_id"], name: "index_products_on_brand_id"
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
@@ -69,5 +77,6 @@ ActiveRecord::Schema.define(version: 2022_01_26_210856) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "products", "brands"
   add_foreign_key "products", "categories"
 end
