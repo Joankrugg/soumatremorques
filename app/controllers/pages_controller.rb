dade2a390e53c11b07c970e2b87d1bf782162c53
Philippe chambon <joan_krugg@live.fr>
2022-03-10 16:54:22 +0100

    main change in design

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,27 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!
 
   def home
     @products = Product.all
+  end
+
+  def articles
+
+  end
+
+  def holydays
+
+  end
+
+  def horses
+
+  end
+
+  def smart
+
+  end
+
+  def constant_gardener
+
   end
 end
