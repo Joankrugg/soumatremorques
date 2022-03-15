e71ee34f5f4c15ae27d86aa7ff684817759018ea
Philippe Chambon <joan_krugg@live.fr>
2022-03-15 21:38:41 +0100

    horses page

--- config/routes.rb
+++ config/routes.rb
@@ -77,5 +77,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'pages/articles'
   get 'pages/holydays'
+  get 'pages/horses'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
