dea1480e9fe5d81968769431750904a73082c602
Philippe Chambon <joan_krugg@live.fr>
2022-03-16 09:21:44 +0100

    pages content

--- config/routes.rb
+++ config/routes.rb
@@ -75,8 +75,9 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root to: 'pages#home'
-  get 'pages/articles'
   get 'pages/holidays'
   get 'pages/horses'
+  get 'pages/constant_gardener'
+  get 'pages/smart'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
